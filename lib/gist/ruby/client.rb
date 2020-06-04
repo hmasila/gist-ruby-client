902ca0ca05074d638bf8a1ada9f5da2eef3ef055
Hannah Masila <hannahmasila@gmail.com>
2020-06-04 21:06:33 +0300

    initial connit

---
+++ lib/gist/ruby/client.rb
@@ -0,0 +1,10 @@
+require "gist/ruby/client/version"
+
+module Gist
+  module Ruby
+    module Client
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/gist/ruby/client/version.rb
@@ -0,0 +1,7 @@
+module Gist
+  module Ruby
+    module Client
+      VERSION = "0.1.0"
+    end
+  end
+end
